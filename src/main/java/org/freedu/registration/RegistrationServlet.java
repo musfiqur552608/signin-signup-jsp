e3e2044c244a5769fd69051c806b3d5cfcc844fb
Musfiqur Rahman <mushfiqur15-10872@diu.edu.bd>
2023-05-28 15:26:12 +0600

    Registration form done

--- src/main/java/org/freedu/registration/RegistrationServlet.java
+++ src/main/java/org/freedu/registration/RegistrationServlet.java
@@ -1,5 +1,6 @@
 package org.freedu.registration;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,6 +8,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @WebServlet("/register")
 public class RegistrationServlet extends HttpServlet {
@@ -14,8 +19,42 @@ public class RegistrationServlet extends HttpServlet {
        
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname = request.getParameter("name");
+		String uemail = request.getParameter("email");
+		String upwd = request.getParameter("pass");
+		String umobile = request.getParameter("contact");
 		PrintWriter out = response.getWriter();
-		out.print("Woring");
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dipti", "root", "");
+			PreparedStatement pst = con.prepareStatement("insert into users(uname,upwd, uemail, umobile) values(?,?,?,?)");
+			pst.setString(1, uname);
+			pst.setString(2, upwd);
+			pst.setString(3, uemail);
+			pst.setString(4, umobile);
+			
+			int rowCount = pst.executeUpdate();
+			
+			RequestDispatcher dispatcher = null;
+			
+			dispatcher = request.getRequestDispatcher("login.jsp");
+			
+			if(rowCount>0) {
+				request.setAttribute("status", "success");
+			}else {
+				request.setAttribute("status", "failed");
+			}
+			dispatcher.forward(request, response);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
